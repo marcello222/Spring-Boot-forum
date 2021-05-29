51985ffc39172322f67e172f025a7e7be78890a7
HP <HP@192.168.15.7>
2021-05-29 18:23:26 -0300

    Primeiro commit

---
+++ forum/src/main/java/br/com/alura/forum/controller/repository/CursoRepository.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum.controller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Curso;
+
+
+	public interface CursoRepository extends JpaRepository<Curso, Long> {
+			
+
+	public Curso findByNome(String nomeCurso);
+}
